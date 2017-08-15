5ad183bca85f40cd8bd0b4c04fa9111cbb758a99
Cauê <cauemateus@hotmail.com>
2017-08-15 15:19:38 -0300

    arquivos necessário do ex1 trabalho1 criados

--- Trabalho1/Ex1/main.c
+++ Trabalho1/Ex1/main.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "circulo.h"
+#include "raio.h"
+#include "calculo_raio.h"
+#include "centro_circulo.h"
+#include "entrada.c"
+#include "saida.c"
 
 int main()
 {
