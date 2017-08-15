0fe17ad2e7022a88d337ed5beba8bc2baae17c4d
Cauê <cauemateus@hotmail.com>
2017-08-15 15:25:23 -0300

    corrigindo erro de arquivos

--- Trabalho1/Ex1/main.c
+++ Trabalho1/Ex1/main.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "circulo.h"
-#include "raio.h"
-#include "calculo_raio.h"
-#include "centro_circulo.h"
-#include "entrada.c"
-#include "saida.c"
 
 int main()
 {
