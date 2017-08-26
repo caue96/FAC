5bdb90e54bafadeb2d91c24e2895c4fdb2c2bbda
elissliima <eliss.liima@gmail.com>
2017-08-26 19:01:29 -0300

    Update main.c
    Ex2 feito :)

--- Trabalho1/Ex2/main.c
+++ Trabalho1/Ex2/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc,char *argv[])
 {
+   int i;
+   printf("# de parametros:%d\n",(argc-1));
+	
+   for(i=0;i<argc;i++){
+	   if(i==0){
+		   printf("Nome do executavel:%s\n",argv[i]);
+	   }else{
+		   printf("Parametro #%d: %s \n",i,argv[i]);
+	   
 	return 0;
 }
